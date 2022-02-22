fdabf02c33883846a88d459397bb801ffcb3c763
Wilton Souza <willsouzafilho@gmail.com>
2022-02-22 15:38:39 -0300

    Creating the project and adding the create restaurant route

---
+++ config/config.exs
@@ -0,0 +1,46 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+import Config
+
+config :inmana,
+  ecto_repos: [Inmana.Repo]
+
+config :inmana, Inmana.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
+# Configures the endpoint
+config :inmana, InmanaWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: InmanaWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Inmana.PubSub,
+  live_view: [signing_salt: "7Uch4xMv"]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :inmana, Inmana.Mailer, adapter: Swoosh.Adapters.Local
+
+# Swoosh API client is needed for adapters other than SMTP.
+config :swoosh, :api_client, false
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
